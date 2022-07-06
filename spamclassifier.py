deb5d58d9fdbdaea31890891ec75cc38f7303a2d
enricosuarve <HLKjh*&&*^&*fhjvjhcjhFDf^%&>
2022-07-06 18:16:57 +0100

    Initial commit

---
+++ spamclassifier.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+training_spam = np.loadtxt(open("data/training_spam.csv"), delimiter=",").astype(int)
+print("Shape of the spam training data set:", training_spam.shape)
+print(training_spam)
+
+testing_spam = np.loadtxt(open("data/testing_spam.csv"), delimiter=",").astype(int)
+print("Shape of the spam testing data set:", testing_spam.shape)
+print(testing_spam)
+
+
+# This skeleton code simply classifies every input as ham
+#
+# Here you can see there is a parameter k that is unused, the
+# point is to show you how you could set up your own. You might
+# also pass in extra data via a train method (also does nothing
+#  here). Modify this code as much as you like so long as the
+# accuracy test in the cell below runs.
+
+class SpamClassifier:
+    def __init__(self, k):
+        self.k = k
+
+    def train(self):
+        pass
+
+    def predict(self, data):
+        return np.zeros(data.shape[0])
+
+
+def create_classifier():
+    classifier = SpamClassifier(k=1)
+    classifier.train()
+    return classifier
+
+
+classifier = create_classifier()
+
+
+def run_tests():
+    SKIP_TESTS = False
+
+    if not SKIP_TESTS:
+        testing_spam = np.loadtxt(open("data/testing_spam.csv"), delimiter=",").astype(int)
+        test_data = testing_spam[:, 1:]
+        test_labels = testing_spam[:, 0]
+
+        predictions = classifier.predict(test_data)
+        accuracy = np.count_nonzero(predictions == test_labels) / test_labels.shape[0]
+        print(f"Accuracy on test data is: {accuracy}")
+
+
+run_tests()
